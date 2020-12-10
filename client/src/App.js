d33e62c7c9178f48d7af649c87e93c90166a8c45
Matt Bongiorno <bigiorno@gmail.com>
2020-12-10 10:57:48 -0500

    header/setup

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Header from "./components/header";
+import Search from "./components/search";
+import Saved from "./components/saved";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App container-fluid">
+        <div className="row navigation navbar navbar-light bg-light">
+          <div className=" col-12 navigation-sub">
+            <Link to="/" className="item navbar-brand">Search</Link>
+            <Link to="/saved" className="item navbar-brand">Saved</Link>
+          </div>
+        </div>
+        <Header />
+
+        <Route exact path="/" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
---
+++ client/src/components/book/Book.js
@@ -0,0 +1,44 @@
+import React from "react";
+import API from "../../utils/API";
+
+const Book = ({ volumeInfo }) => {
+    function handleBooksubmit(event) {
+        // event.preventDefault();
+        if (volumeInfo.title) {
+            API.saveBook(
+                {
+                    title: volumeInfo,
+                    authors: volumeInfo.authors,
+                    description: volumeInfo.description,
+                    image: volumeInfo.imageLinks.thumbnail,
+                    link: volumeInfo.infoLink
+                }
+            )
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+        }
+        // console.log(volumeInfo);
+    };
+    return (
+        <div className="col-12">
+            <div className="card">
+                <div className="row justify-content-center">
+                <button className="save-btn col-3 m-3" onClick={handleBooksubmit}>Save</button>
+            <button className="view-btn col-3 m-3"><a href={volumeInfo.infoLink} target="_blank">View</a></button>
+                </div>
+           
+                <p>Title: {volumeInfo.title}</p>
+                <p>Author(s): {volumeInfo.authors}</p>
+                <p>Description: {volumeInfo.description || "No description available"}</p>
+                <div>
+                <img src= {volumeInfo.imageLinks?.thumbnail || "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/100px-No_image_available.svg.png"} alt="book cover" />
+                </div>
+                {/* <p>Link: <a href={volumeInfo.infoLink} target="_blank">{volumeInfo.title}</a> </p> */}
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Book;
